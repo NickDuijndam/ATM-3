2b41c2e10685b9655ec117e63bcd14ca1db722f1
Nick Duijndam <nickduijndam@hotmail.com>
2019-08-16 01:27:25 +0200

    Fix CI

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM alpine:3.10
 
 WORKDIR /github/workspace
 
-RUN apk add curl wget bash jq tar
+RUN apk add curl wget bash jq zip python3
 
 ENTRYPOINT ["bash", "./deploy.sh"]
--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko
 manifest=$(cat manifest.json)
 cd overrides/mods
 
-for line in $(cat override_mods.txt);
-do
-    wget --content-disposition --user-agent="$userAgent" $line 2>/dev/null
-done
+python3 dl_overrides.py
 
 jq -c '.files[]' ../../manifest.json | while read i
 do
@@ -18,17 +15,45 @@ done
 name="$(jq -r '.name' <<< $manifest)__$(jq -r '.version' <<< $manifest).zip"
 name=${name// /_}
 cd ../
-tar --exclude='override_mods.txt' --exclude='dl_overrides.py' -cf  ../$name *
-ls -la
+zip -q -x '*override_mods.txt*' -x '*dl_overrides.py*' -r "../$name" .
+cd ../
 
 curl -X POST https://api.dropboxapi.com/2/files/delete_v2 \
     --header "Authorization: Bearer $DROPBOX_TOKEN" \
     --header "Content-Type: application/json" \
     --data "{\"path\": \"/ATM3\"}"
 
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer $DROPBOX_TOKEN" \
-    --header "Dropbox-API-Arg: {\"path\": \"/ATM3/${name}\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary "$name"
+split -b 131072000 -a 3 "$name" "$name."
+files=(./$name.*)
+total=${#files[@]}
+counter=1
+uploaded=0
 
+for filename in ./$name.*; do
+	if [ $counter = 1 ]; then
+	   	result=$(wget -qO- \
+			--header="Authorization: Bearer $DROPBOX_TOKEN" \
+			--header="Dropbox-API-Arg: {\"close\": false}" \
+			--header="Content-Type: application/octet-stream" \
+			--post-file $filename https://content.dropboxapi.com/2/files/upload_session/start)
+
+		sessionId="$(jq -r '.session_id' <<< $result)"
+	elif [ $counter = $total ]; then
+	   	wget \
+			--header="Authorization: Bearer $DROPBOX_TOKEN" \
+			--header "Dropbox-API-Arg: {\"cursor\": {\"session_id\": \"$sessionId\",\"offset\": $uploaded},\"commit\": {\"path\": \"/ATM3/$name\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}}" \
+			--header="Content-Type: application/octet-stream" \
+			--post-file $filename https://content.dropboxapi.com/2/files/upload_session/finish
+
+	else
+		echo $uploaded
+		wget \
+			--header="Authorization: Bearer $DROPBOX_TOKEN" \
+			--header "Dropbox-API-Arg: {\"cursor\": {\"session_id\": \"$sessionId\",\"offset\": $uploaded},\"close\": false}" \
+			--header="Content-Type: application/octet-stream" \
+			--post-file $filename https://content.dropboxapi.com/2/files/upload_session/append_v2
+	fi
+
+	uploaded=$(expr $uploaded + $(wc -c < $filename))
+	counter=$(expr $counter + 1)
+done
